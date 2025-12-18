0e00953d72782afa895116fc11cb59a472f8bde3
nfons <natefonseka@gmail.com>
2025-12-18 14:05:16 -0500

    feat(putility): implement DTOs, service, and module for PUtility management

---
+++ src/entities/putility/putlity.service.ts
@@ -0,0 +1,29 @@
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PUtility } from './putility.entity';
+
+@Injectable()
+export class PutlityService {
+    constructor(
+        @InjectRepository(PUtility)
+        private PUtilityRepository: Repository<PUtility>,
+    ) {}
+
+    findAll(): Promise<PUtility[]> {
+        return this.PUtilityRepository.find();
+    }
+
+    findOne(id: number): Promise<PUtility | null> {
+        return this.PUtilityRepository.findOneBy({ id });
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.PUtilityRepository.delete(id);
+    }
+
+    async add(PUtility: PUtility): Promise<PUtility> {
+        return this.PUtilityRepository.save(PUtility);
+    }
+}
