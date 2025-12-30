b2137f3a0310968c3b2c97dcd6b97dc6f326a962
nfons <natefonseka@gmail.com>
2025-12-29 20:05:05 -0500

    adding swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const port: number = Number.parseInt(process.env.PORT ?? '', 10) || 8080;
 
@@ -7,6 +8,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Power Switch')
+    .setDescription('powerswitch api')
+    .setVersion('1.0')
+    .addTag('local')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory)
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
