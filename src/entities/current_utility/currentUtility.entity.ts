193b3cf5a5a67144e0be53c779eac9c6b1aa806d
nfons <natefonseka@gmail.com>
2026-01-14 19:54:08 -0500

    Unit tests

--- src/entities/current_utility/currentUtility.entity.ts
+++ src/entities/current_utility/currentUtility.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { utilityType } from '../../entities/utlityType.enum';
 
 @Entity()
